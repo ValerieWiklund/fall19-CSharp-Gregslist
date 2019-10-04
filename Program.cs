d52a1705ec09d7e1e1e252fb0948c0736a847b43
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-10-04 09:58:39 -0600

    start:

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gregslist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
