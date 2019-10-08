544b0b2a8e3875f20836ef002b5855ce2b0725dd
Valerie Wiklund <vkwiklund@gmail.com>
2019-10-08 16:15:55 -0600

    back and forths

--- Controllers/ListingController.cs
+++ Controllers/ListingController.cs
@@ -4,71 +4,72 @@ using gregslist.Services;
 
 namespace gregslist.Controllers
 {
-    public class CarController
+  public class CarController
+  {
+    private CarService _carService { get; set; } = new CarService();
+    public void UserInput()
     {
-        private CarService _carService { get; set; } = new CarService();
-        public void UserInput()
-        {
-            _carService.GetCars();
+      _carService.GetCars();
+      Print();
+      string choice = Console.ReadLine().ToLower();
+      Console.Clear();
+      switch (choice)
+      {
+        case "q":
+          Environment.Exit(1);
+          break;
+        default:
+          if (int.TryParse(choice, out int index))
+          {
+            //print car details
+            _carService.GetCars(index - 1);
             Print();
-            string choice = Console.ReadLine().ToLower();
+            Console.ReadKey();
             Console.Clear();
-            switch (choice)
-            {
-                case "q":
-                    Environment.Exit(1);
-                    break;
-                default:
-                    if (int.TryParse(choice, out int index))
-                    {
-                        //print car details
-                        _carService.GetCars(index - 1);
-                        Print();
-                        Console.ReadKey();
-                        Console.Clear();
-                    }
-                    else
-                    {
-                        System.Console.WriteLine("Invalid Command!");
-                        string f = "Failure";
-                        bool color = true;
-                        string message = "";
-                        foreach (char c in f)
-                        {
-                            if (color)
-                            {
-                                Console.BackgroundColor = ConsoleColor.Magenta;
-                                Console.ForegroundColor = ConsoleColor.Green;
-                            }
-                            else
-                            {
-                                Console.BackgroundColor = ConsoleColor.Green;
-                                Console.ForegroundColor = ConsoleColor.Magenta;
-                            }
-                            color = !color;
-                            Console.Clear();
-                            message += c;
-                            Console.WriteLine(message);
-                            Console.Beep();
-                        }
-                        Thread.Sleep(1000);
-                        Console.ResetColor();
-                        Console.Clear();
-                    }
-                    break;
+          }
+          else
+          {
+            System.Console.WriteLine("Invalid Command!");
+            //     string f = "Failure";
+            //     bool color = true;
+            //     string message = "";
+            //     foreach (char c in f)
+            //     {
+            //         if (color)
+            //         {
+            //             Console.BackgroundColor = ConsoleColor.Magenta;
+            //             Console.ForegroundColor = ConsoleColor.Green;
+            //         }
+            //         else
+            //         {
+            //             Console.BackgroundColor = ConsoleColor.Green;
+            //             Console.ForegroundColor = ConsoleColor.Magenta;
+            //         }
+            //         color = !color;
+            //         Console.Clear();
+            //         message += c;
+            //         Console.WriteLine(message);
+            //         Console.Beep();
+            //     }
+            //     Thread.Sleep(1000);
+            //     Console.ResetColor();
+            //     Console.Clear();
+            // }
 
-            }
-        }
-
-        private void Print()
-        {
-            foreach (string message in _carService.Messages)
-            {
-                System.Console.WriteLine(message);
-            }
-            _carService.Messages.Clear();
-            System.Console.WriteLine();
-
-        }
+          }
+          break;
+      }
     }
+
+    private void Print()
+    {
+      foreach (string message in _carService.Messages)
+      {
+        System.Console.WriteLine(message);
+      }
+      _carService.Messages.Clear();
+      System.Console.WriteLine();
+
+    }
+  }
 }
--- Services/ListingService.cs
+++ Services/ListingService.cs
@@ -4,58 +4,58 @@ using gregslist.Models;
 
 namespace gregslist.Services
 {
-    public class CarService
+  public class CarService
+  {
+    public List<Listing> Listings { get; private set; }
+    public List<string> Messages { get; set; }
+
+    public void Setup()
     {
-        public List<Listing> Listings { get; private set; }
-        public List<string> Messages { get; set; }
+      Car ford = new Car("Ford", "F350", 2019, "leather seats and all the bells and whistles", 50000.89, "New", "whatever", "truck", "gas");
+      Car honda = new Car("Honda", "Civic", 1989, "how dare you", 1000, "Sorta New", "whatever", "car", "gas");
+      Car toyota = new Car("Toyota", "Tacoma", 2011, "full of trash", 12000, "old", "whatever", "truck", "gas");
+      Car tracker = new Car("Darryl", "Frankenstein", 1989, "mostly a tracker", 1000000, "New Parts....", "whatever", "truck", "gas");
 
-        public void Setup()
-        {
-            Car ford = new Car("Ford", "F350", 2019, "leather seats and all the bells and whistles", 50000.89, "New", "whatever", "truck", "gas");
-            Car honda = new Car("Honda", "Civic", 1989, "how dare you", 1000, "Sorta New", "whatever", "car", "gas");
-            Car toyota = new Car("Toyota", "Tacoma", 2011, "full of trash", 12000, "old", "whatever", "truck", "gas");
-            Car tracker = new Car("Darryl", "Frankenstein", 1989, "mostly a tracker", 1000000, "New Parts....", "whatever", "truck", "gas");
+      Furniture couch = new Furniture(2011, "its a brown leather couch", 100, "Its very cracked and warn", "n______n", "Couch");
 
-            Furniture couch = new Furniture(2011, "its a brown leather couch", 100, "Its very cracked and warn", "n______n", "Couch");
-
-            Listings.AddRange(new Listing[] { ford, honda, toyota, tracker, couch });
-        }
-
-        internal void GetCars(int index)
-        {
-            if (index < Listings.Count && index > -1)
-            {
-                Listing listing = Listings[index];
-                Messages.Add(listing.GetTemplate());
-                if (listing is Car)
-                {
-                    Car carListing = (Car)listing;
-                    carListing.HaveMechanicChecked();
-                }
-            }
-            else
-            {
-                Messages.Add("Invalid Choice... Dummy!");
-
-            }
-        }
-
-        public void GetCars()
-        {
-            Messages.Add("Available Cars");
-            for (int i = 0; i < Listings.Count; i++)
-            {
-                Listing c = Listings[i];
-                Messages.Add($"{i + 1}: {c.ImgUrl} - {c.Price}");
-            }
-            Messages.Add("Type a number to see details or Q to quit");
-        }
-
-        public CarService()
-        {
-            Messages = new List<string>();
-            Listings = new List<Listing>();
-            Setup();
-        }
+      Listings.AddRange(new Listing[] { ford, honda, toyota, tracker, couch });
     }
+
+    internal void GetCars(int index)
+    {
+      if (index < Listings.Count && index > -1)
+      {
+        Listing listing = Listings[index];
+        Messages.Add(listing.GetTemplate());
+        if (listing is Car)
+        {
+          Car carListing = (Car)listing;
+          Messages.Add($"car was checked by mechanic: {carListing.HaveMechanicChecked()}");
+        }
+      }
+      else
+      {
+        Messages.Add("Invalid Choice... Dummy!");
+
+      }
+    }
+
+    public void GetCars()
+    {
+      Messages.Add("Available Cars");
+      for (int i = 0; i < Listings.Count; i++)
+      {
+        Listing c = Listings[i];
+        Messages.Add($"{i + 1}: {c.ImgUrl} - {c.Price}");
+      }
+      Messages.Add("Type a number to see details or Q to quit");
+    }
+
+    public CarService()
+    {
+      Messages = new List<string>();
+      Listings = new List<Listing>();
+      Setup();
+    }
+  }
 }
